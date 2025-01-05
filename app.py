adc6b1aa47aa0ae6b93d2d177d648330a8c431ba
madhavb1 <148400086+madhavb1@users.noreply.github.com>
2025-01-05 04:09:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ import pickle
 model = tf.keras.models.load_model('model.h5')
 
 # Load the encoders and scaler
-with open('label_encoder_gender.pkl', 'rb') as file:
+with open('D:\ANN_Classification\label_encoder_gender.pk1', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('onehot_encoder_geo.pkl', 'rb') as file:
+with open('D:\ANN_Classification\onehot_encoder_geo.pk1', 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
+with open('D:\ANN_Classification\sscaler.pk1', 'rb') as file:
     scaler = pickle.load(file)
 
 
